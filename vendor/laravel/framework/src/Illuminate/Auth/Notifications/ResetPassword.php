cdfb0a4d6531de5c7a0dfcd7cfa86e43501c9a02
redhero1996 <frog.dbsk.cass@gmail.com>
2019-07-06 17:10:49 +0700

    change gitignore

--- vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -2,9 +2,9 @@
 
 namespace Illuminate\Auth\Notifications;
 
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class ResetPassword extends Notification
 {
@@ -59,7 +59,7 @@ class ResetPassword extends Notification
         return (new MailMessage)
             ->subject(Lang::getFromJson('Reset Password Notification'))
             ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::getFromJson('Reset Password'), url(config('app.url') . route('password.reset', $this->token, false)))
+            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', $this->token, false)))
             ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
     }
 
